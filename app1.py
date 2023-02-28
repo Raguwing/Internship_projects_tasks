944cf0877b571654da52413e606e91b687d03646
RAGUWING GUDLA <121440415+Raguwing@users.noreply.github.com>
2023-02-28 16:10:50 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.header("Data Science Internship feb 2023!")
+st.header("Hello, I am Gudla Raguwing👋")
+st.header("Lets connect!")
+if st.button('Linkdin'):
+    st.write("https://www.linkedin.com/in/raguwing-gudla/")
+else: 
+    st.write()
+
+if st.button('Github'):
+    st.write("https://github.com/Raguwing")
+else:
+    st.write()
